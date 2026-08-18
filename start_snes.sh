d99ffd628b18c207dc5c9aaa80e2b641b74d8456
can <can@debian-fb.myguest.virtualbox.org>
2026-08-18 16:52:43 +0700

    fix start_snes.sh: update stale paths to /mnt/data/src, add shebang

--- start_snes.sh
+++ start_snes.sh
@@ -1,1 +1,2 @@
-~/src/rustsdlretro/target/release/rustsdlretro-frontend ~/src/libretro-fceumm/fceumm_libretro.so ~/rom/nes/Contra\ \(USA\).nes
+#!/bin/sh
+/mnt/data/src/rustsdlretro/target/release/rustsdlretro-frontend /mnt/data/src/libretro-fceumm/fceumm_libretro.so ~/rom/nes/Contra\ \(USA\).nes
