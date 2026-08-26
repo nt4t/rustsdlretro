4c42d2d2da2695de6eab4633e5c8549993caace2
nt4t <nt4ttt@gmail.com>
2026-08-26 10:43:25 +0700

    Delete start_nes.sh

--- start_nes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# NES launcher - supports both .nes and .zip files
-# For ZIP archives, just replace the ROM path with your .zip file:
-#   ./target/release/rustsdlretro-frontend <core.so> game.zip
-
-./target/release/rustsdlretro-frontend \
-    ~/src/libretro-fceumm/fceumm_libretro.so \
-    "/mnt/d/Games/Roms/nes/Contra\ \(USA\).zip"
