ada0c753315cad6a14f733031b182a536297db4c
nt4t <nt4ttt@gmail.com>
2026-08-26 10:43:11 +0700

    Delete start_gen.sh

--- start_gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Genesis launcher - supports both .gen/.md and .zip files
-# For ZIP archives, just replace the ROM path with your .zip file:
-#   ./target/release/rustsdlretro-frontend <core.so> game.zip
-
-./target/release/rustsdlretro-frontend \
-    ~/src/Genesis-Plus-GX/genesis_plus_gx_libretro.so \
-    "/mnt/d/Games/Roms/gen/MUSHA - Metallic Uniframe Super Hybrid Armor (USA).zip"
